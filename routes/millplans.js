812e6263bda6387a2162aca2fd20ae206a0dd477
Joy Carlson <63269555+joynewwork@users.noreply.github.com>
2020-12-15 13:44:40 -1000

    Add files via upload

--- routes/millplans.js
+++ routes/millplans.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var MillPlans = require('../models/MillPlan');
+var MillPlan = require('../models/MillPlan')
+
+const mongoose = require('mongoose');
 
 /* GET home page. */
-
-router.get('/', async function (req, res, next) {
+router.get('/', async function(req, res, next) {
     try {
-        const millplans = await MillPlans.find();
-
-        res.status(200).json({
-            data: { millplans }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
-});
-
-
-router.post('/add', async function (req, res) {
-    try {
-        const newMillPlan = await MillPlan.create(req.body);
+        const MillPlans = await MillPlan.find();
     
-        res.status(201).json({
-          status: 'success',
-          data: { millplan: newMillPlan }
+        res.status(200).json({
+          data: { MillPlans }
         });
       } catch (err) {
-        res.status(400).json({
+        res.status(404).json({
           status: 'fail',
           message: err
         });
@@ -37,17 +21,68 @@ router.post('/add', async function (req, res) {
 });
 
 
-/*
-router.get('/id', function (req, res, next) {
-    res.json('MillPlansId');
+router.get('/:id', async function(req, res, next) {
+    try { 
+
+        const MillPlans = await MillPlan.findById(req.params.id);
+    
+        res.status(200).json({
+          data: { MillPlans }
+        });
+      } catch (err) {
+        res.status(404).json({
+          status: 'fail',
+          message: err
+        });
+      }    
 });
 
-router.get('/update', function (req, res, next) {
-    res.json('MillPlansUpdate');
-});
+router.post('/add', async function(req, res, next) {
+    try {
+        const newMillPlan = await MillPlan.create(req.body);
+    
+        res.status(201).json({
+          data: { MillPlans: newMillPlan }
+        });
+      } catch (err) {
+        res.status(400).json({
+          status: 'fail',
+          message: err
+        });
+    };
+    });
+    
+    router.put('/update/:id', async function(req, res) {
+        try {
+            const MillPlans = await MillPlan.findByIdAndUpdate(req.params.id, req.body, {
+              new: true,
+              runValidators: true
+            });
+        
+            res.status(200).json({
+              data: { MillPlans }
+            });
+          } catch (err) {
+            res.status(500).json({
+              status: 'fail',
+              message: err
+            });
+          }
+        });
+    
+        router.delete('/delete/:id', async function(req, res, next) {
+            try {
+                await MillPlan.findByIdAndDelete(req.params.id);
+                res.status(204).json({
+                  data: null
+                });
+              } catch (err) {
+                res.status(404).json({
+                  status: 'fail',
+                  message: err
+                });
+              }
+            });        
+    
 
-router.get('/delete', function (req, res, next) {
-    res.json('MillPlansDelete');
-});
-*/
 module.exports = router;
