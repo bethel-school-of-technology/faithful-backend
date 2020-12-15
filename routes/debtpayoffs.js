db53cce798b5aa614fcb5ca91ba1059eacce2501
Joy Carlson <63269555+joynewwork@users.noreply.github.com>
2020-12-15 13:43:06 -1000

    Add files via upload

--- routes/debtpayoffs.js
+++ routes/debtpayoffs.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var DebtPayoffs = require('../models/DebtPayoff');
+var DebtPayoff = require('../models/DebtPayoff');
 
 /* GET home page. */
 
 router.get('/', async function (req, res, next) {
     try {
-        const debtpayoffs = await DebtPayoffs.find();
+        const debtpayoffs = await DebtPayoff.find();
 
         res.status(200).json({
             data: { debtpayoffs }
@@ -20,33 +20,67 @@ router.get('/', async function (req, res, next) {
 });
 
 
-router.post('/add', async function (req, res) {
+router.get('/:id', async function(req, res, next) {
+    try { 
+
+        const DebtPayoffs = await DebtPayoff.findById(req.params.id);
+    
+        res.status(200).json({
+          data: { DebtPayoffs }
+        });
+      } catch (err) {
+        res.status(404).json({
+          status: 'fail',
+          message: err
+        });
+      }    
+});
+
+router.post('/add', async function(req, res, next) {
     try {
         const newDebtPayoff = await DebtPayoff.create(req.body);
     
         res.status(201).json({
-          data: { debtpayoff: newDebtPayoff }
+          data: { DebtPayoffs: newDebtPayoff }
         });
       } catch (err) {
         res.status(400).json({
           status: 'fail',
           message: err
         });
-      }
-});
-
-
-/*
-router.get('/id', function (req, res, next) {
-    res.json('MillPlansId');
-});
-
-router.get('/update', function (req, res, next) {
-    res.json('MillPlansUpdate');
-});
-
-router.get('/delete', function (req, res, next) {
-    res.json('MillPlansDelete');
-});
-*/
+    };
+    });
+    
+    router.put('/update/:id', async function(req, res) {
+        try {
+            const DebtPayoffs = await DebtPayoff.findByIdAndUpdate(req.params.id, req.body, {
+              new: true,
+              runValidators: true
+            });
+        
+            res.status(200).json({
+              data: { MillPlans }
+            });
+          } catch (err) {
+            res.status(500).json({
+              status: 'fail',
+              message: err
+            });
+          }
+        });
+    
+        router.delete('/delete/:id', async function(req, res, next) {
+            try {
+                await DebtPayoff.findByIdAndDelete(req.params.id);
+                res.status(204).json({
+                  data: null
+                });
+              } catch (err) {
+                res.status(404).json({
+                  status: 'fail',
+                  message: err
+                });
+              }
+            });        
+    
 module.exports = router;
