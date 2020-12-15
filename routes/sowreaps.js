4b2f9597730403035b8569f0df716a32081c9813
Joy Carlson <63269555+joynewwork@users.noreply.github.com>
2020-12-15 13:45:59 -1000

    Add files via upload

--- routes/sowreaps.js
+++ routes/sowreaps.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var SowReaps = require('../models/SowReap');
+var SowReap = require('../models/SowReap');
 
 /* GET home page. */
 
 router.get('/', async function (req, res, next) {
     try {
-        const sowreaps = await SowReaps.find();
+        const sowreaps = await SowReap.find();
 
         res.status(200).json({
             data: { sowreaps }
@@ -20,33 +20,68 @@ router.get('/', async function (req, res, next) {
 });
 
 
-router.post('/add', async function (req, res) {
+router.get('/:id', async function(req, res, next) {
+    try { 
+
+        const SowReaps = await SowReap.findById(req.params.id);
+    
+        res.status(200).json({
+          data: { SowReaps }
+        });
+      } catch (err) {
+        res.status(404).json({
+          status: 'fail',
+          message: err
+        });
+      }    
+});
+
+router.post('/add', async function(req, res, next) {
     try {
         const newSowReap = await SowReap.create(req.body);
     
         res.status(201).json({
-          data: { sowreap: newSowReap }
+          data: { SowReaps: newSowReap }
         });
       } catch (err) {
         res.status(400).json({
           status: 'fail',
           message: err
         });
-      }
-});
+    };
+    });
+    
+    router.put('/update/:id', async function(req, res) {
+        try {
+            const SowReaps = await SowReap.findByIdAndUpdate(req.params.id, req.body, {
+              new: true,
+              runValidators: true
+            });
+        
+            res.status(200).json({
+              data: { SowReaps }
+            });
+          } catch (err) {
+            res.status(500).json({
+              status: 'fail',
+              message: err
+            });
+          }
+        });
+    
+        router.delete('/delete/:id', async function(req, res, next) {
+            try {
+                await SowReap.findByIdAndDelete(req.params.id);
+                res.status(204).json({
+                  data: null
+                });
+              } catch (err) {
+                res.status(404).json({
+                  status: 'fail',
+                  message: err
+                });
+              }
+            });        
+    
 
-
-/*
-router.get('/id', function (req, res, next) {
-    res.json('MillPlansId');
-});
-
-router.get('/update', function (req, res, next) {
-    res.json('MillPlansUpdate');
-});
-
-router.get('/delete', function (req, res, next) {
-    res.json('MillPlansDelete');
-});
-*/
 module.exports = router;
